ff6594972f0ec3a4314a90befd4c98410a55acd7
mikezks <23002661+mikezks@users.noreply.github.com>
2023-09-15 16:58:03 +0200

    feat: signals

--- libs/tickets/feature-booking/src/lib/flight-search/flight-search.component.ts
+++ libs/tickets/feature-booking/src/lib/flight-search/flight-search.component.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component, effect } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { FormsModule } from '@angular/forms';
 import { CityPipe } from '@flight-demo/shared/ui-common';
 import { Flight, injectTicketsFeature } from '@flight-demo/tickets/domain';
 import { FlightCardComponent } from '../flight-card/flight-card.component';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-flight-search',
@@ -15,9 +17,16 @@ import { FlightCardComponent } from '../flight-card/flight-card.component';
 export class FlightSearchComponent {
   protected ticketsFeature = injectTicketsFeature();
 
-  from = 'Graz';
-  to = 'Hamburg';
+  from = signal('Graz');
+  to = signal('Hamburg');
   selectedFlight: Flight | undefined;
+  lazyFrom$ = toObservable(this.from).pipe(
+    debounceTime(300)
+  );
+  lazyFrom = toSignal(this.lazyFrom$, {
+    initialValue: this.from()
+  });
+  flightRoute = computed(() => 'From ' + this.lazyFrom() + ' to ' + this.to() + '.');
 
   basket: Record<number, boolean> = {
     3: true,
